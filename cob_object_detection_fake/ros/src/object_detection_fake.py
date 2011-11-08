85dc4debae0410abcfa1877a443eb3ea9363165b
ipa-jsf-mf <matthias-freier@gmx.de>
2011-11-08 16:32:03 +0100

    rename srvices

--- cob_object_detection_fake/ros/src/object_detection_fake.py
+++ cob_object_detection_fake/ros/src/object_detection_fake.py
@@ -11,38 +11,38 @@ from gazebo.srv import *
 
 def handle_detect_object(req):
 	
-	srv_set_model_state = rospy.ServiceProxy('gazebo/get_world_properties', GetWorldProperties)
-	res_set = srv_set_model_state()
+	srv_get_world_properties = rospy.ServiceProxy('gazebo/get_world_properties', GetWorldProperties)
+	res_world = srv_get_world_properties()
 
 	
 	names=[]
-	for name in res_set.model_names:
+	for name in res_world.model_names:
 		if name == req.object_name.data:
 			names.append(name)
 			
 	if len(names)==0:
 		rospy.logwarn("%s not in database, searching for all objects", req.object_name.data)
-		names = res_set.model_names
+		names = res_world.model_names
 
 	resp = DetectObjectsResponse()
-	
+
 	for name in names:
 		
 		rospy.loginfo("fake detection of %s",name)
 		
-		srv_set_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-		res_set = srv_set_model_state(name,'')
+		srv_get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+		res_model = srv_get_model_state(name,'')
 
-		print res_set
+		print res_model
 
 		srv_get_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-		res_set = srv_get_link_state( "milk::box_body","robot::head_axis_link")
+		res_link = srv_get_link_state("milk::box_body","robot::head_axis_link")
 
-		print res_set
+		print res_link
 
 		detection=Detection()
 		detection.pose.header.frame_id='/head_axis_link'
-		detection.pose.pose = res_set.link_state.pose
+		detection.pose.pose = res_link.link_state.pose
 		detection.label = name
 		
 		resp.object_list.detections.insert(0,detection)
