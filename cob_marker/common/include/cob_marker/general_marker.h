6099549b123279c6527ce8fa33d4d675fd897f87
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2012-08-24 08:22:28 +0200

    cob_marker: added comments

--- cob_marker/common/include/cob_marker/general_marker.h
+++ cob_marker/common/include/cob_marker/general_marker.h
@@ -1,9 +1,59 @@
-/*
- * general_marker.h
- *
- *  Created on: 21.08.2012
- *      Author: josh
- */
+/*!
+*****************************************************************
+* \file
+*
+* \note
+* Copyright (c) 2012 \n
+* Fraunhofer Institute for Manufacturing Engineering
+* and Automation (IPA) \n\n
+*
+*****************************************************************
+*
+* \note
+* Project name: TODO FILL IN PROJECT NAME HERE
+* \note
+* ROS stack name: cob_object_perception
+* \note
+* ROS package name: cob_marker
+*
+* \author
+* Author: Joshua Hampp
+*
+* \date Date of creation: 24.8.2012
+*
+* \brief
+* 6DOF marker
+*
+*****************************************************************
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer. \n
+* - Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution. \n
+* - Neither the name of the Fraunhofer Institute for Manufacturing
+* Engineering and Automation (IPA) nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission. \n
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License LGPL as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License LGPL for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License LGPL along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************/
 
 #ifndef GENERAL_MARKER_H_
 #define GENERAL_MARKER_H_
