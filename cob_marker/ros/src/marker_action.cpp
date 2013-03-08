c39c000c4ae657e13876e3a95acd09c2e1c22fa4
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2013-03-08 20:15:52 +0100

    ready for fuerte and 12.04LTS

--- cob_marker/ros/src/marker_action.cpp
+++ cob_marker/ros/src/marker_action.cpp
@@ -62,7 +62,7 @@
 
 // ROS includes
 #include <ros/ros.h>
-#include <pcl_ros/pcl_nodelet.h>
+#include <nodelet/nodelet.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
@@ -95,7 +95,7 @@ public:
   }
 };
 
-class As_Nodelet : public  pcl_ros::PCLNodelet
+class As_Nodelet : public  nodelet::Nodelet
 {
 protected:
   ros::NodeHandle n_;
@@ -106,7 +106,6 @@ public:
   virtual ~As_Nodelet() {}
 
   void start() {
-    PCLNodelet::onInit();
     n_ = getNodeHandle();
   }
 };
