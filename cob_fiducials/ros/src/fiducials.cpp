35a34e68e7f780095d90749e3a1a21bc5c37239b
ipa-jsf <jsf@ipa.fhg.de>
2013-03-12 14:42:31 +0100

    Implemented ROS interface for fiducial recognition

---
+++ cob_fiducials/ros/src/fiducials.cpp
@@ -0,0 +1,529 @@
+/****************************************************************
+ *
+ * Copyright (c) 2010
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob_object_perception
+ * ROS package name: cob_fiducials
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Jan Fischer, email:jan.fischer@ipa.fhg.de
+ * Supervised by: Jan Fischer, email:jan.fischer@ipa.fhg.de
+ *
+ * Date of creation: March 2013
+ * ToDo:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary rforms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+//##################
+//#### includes ####
+
+// standard includes
+//--
+
+// ROS includes
+#include <ros/ros.h>
+//#include <nodelet/nodelet.h>
+#include <cv_bridge/CvBridge.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
+
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <tf/transform_listener.h>
+#include <cv_bridge/cv_bridge.h>
+
+// ROS message includes
+//#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <visualization_msgs/Marker.h>
+//#include <sensor_msgs/fill_image.h>
+
+// external includes
+//#include <cob_fiducials/fiducialsConfig.h>
+//#include <dynamic_reconfigure/server.h>
+#include <cob_object_detection_msgs/DetectObjects.h>
+#include <cob_vision_utils/GlobalDefines.h>
+#include <cob_vision_utils/VisionUtils.h>
+#include <cob_fiducials/FiducialDefines.h>
+#include <cob_fiducials/FiducialModelPi.h>
+
+#include <boost/thread/mutex.hpp>
+#include <boost/timer.hpp>
+
+//#include "opencv/highgui.h"
+
+using namespace message_filters;
+
+namespace ipa_Fiducials
+{
+
+typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::CameraInfo> ColorImageSyncPolicy;
+
+
+/// @class CobFiducialsNode
+/// This node gathers images from a 'color camera'
+/// to recognize fiducials
+class CobFiducialsNode //: public nodelet::Nodelet
+{
+    enum t_Mode
+    {
+        MODE_TOPIC = 0,
+        MODE_SERVICE,
+        MODE_TOPIC_AND_SERVICE
+    };
+
+
+private:
+    ros::NodeHandle node_handle_;
+
+    boost::shared_ptr<image_transport::ImageTransport> image_transport_;
+
+    // Subscriptions
+    image_transport::SubscriberFilter color_camera_image_sub_;	///< color camera image topic
+    message_filters::Subscriber<sensor_msgs::CameraInfo> color_camera_info_sub_;	///< camera information service
+
+    boost::shared_ptr<message_filters::Synchronizer<ColorImageSyncPolicy > > color_image_sub_sync_; ///< Synchronizer
+    tf::TransformListener transform_listener_; ///< tf transforms
+
+    int sub_counter_; /// Number of subscribers to topic
+    unsigned int endless_counter_; ///< A counter to show that node is still receiving images
+    bool synchronizer_received_; ///< Set to true, when synchronizer fires
+
+    // Service definitions
+    ros::ServiceServer detect_fiducials_service_; ///< Service server to request fidcuial detection
+    // Publisher definitions
+    ros::Publisher detect_fiducials_pub_;
+    ros::Publisher fiducials_marker_publisher_;
+    image_transport::Publisher img2D_pub_; ///< Publishes 2D image data to show detection results
+
+    cv::Mat color_mat_8U3_; 
+    IplImage* color_image_8U3_;	///< Received color image
+    cv::Mat camera_matrix_;
+    bool camera_matrix_initialized_; 
+    //fiducials::fiducialsConfig launch_reconfigure_config_;
+    //dynamic_reconfigure::Server<cob_fiducials::fiducialsConfig> dynamic_reconfigure_server_;
+
+    sensor_msgs::CvBridge cv_bridge_0_; ///< Converts ROS image messages to openCV IplImages
+    static std::string color_image_encoding_; ///< Color encoding of incoming messages
+    CobFiducialsNode::t_Mode ros_node_mode_;	///< Specifys if node is started as topic or service
+    boost::mutex mutexQ_;
+    boost::condition_variable condQ_;
+
+    boost::shared_ptr<ipa_Fiducials::FiducialModelPi> m_pi_tag;
+
+public:
+    /// Constructor.
+    CobFiducialsNode(ros::NodeHandle& nh)
+        : sub_counter_(0),
+          endless_counter_(0),
+          color_image_8U3_(0)
+    {
+        camera_matrix_initialized_ = false;
+        /// Void
+        node_handle_ = nh;
+        onInit();
+    }
+
+    /// Destructor.
+    ~CobFiducialsNode()
+    {
+    }
+
+    void onInit()
+    {
+        /// Create a handle for this node, initialize node
+        //	  node_handle_ = getMTNodeHandle();
+        image_transport_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(node_handle_));
+
+        /// Initialize camera node
+        if (!init()) return;
+    }
+
+    /// Initialize sensor fusion node.
+    /// Setup publisher of point cloud and corresponding color data,
+    /// setup camera toolboxes and colored point cloud toolbox
+    /// @return <code>true</code> on success, <code>false</code> otherwise
+    bool init()
+    {
+        if (loadParameters() == false) return false;
+
+        ros::SubscriberStatusCallback imgConnect    = boost::bind(&CobFiducialsNode::connectCallback, this);
+        ros::SubscriberStatusCallback imgDisconnect = boost::bind(&CobFiducialsNode::disconnectCallback, this);
+
+        // Synchronize inputs of incoming image data.
+        // Topic subscriptions happen on demand in the connection callback.
+        ROS_INFO("[fiducials] Setting up image data subscribers for point cloud mode MODE_SHARED");
+        if (ros_node_mode_ == MODE_TOPIC || ros_node_mode_ == MODE_TOPIC_AND_SERVICE)
+        {
+            detect_fiducials_pub_ = node_handle_.advertise<cob_object_detection_msgs::DetectionArray>("detect_fiducials", 1, imgConnect, imgDisconnect);
+        }
+        if (ros_node_mode_ == MODE_SERVICE || ros_node_mode_ == MODE_TOPIC_AND_SERVICE)
+        {
+            detect_fiducials_service_ = node_handle_.advertiseService("get_fiducials", &CobFiducialsNode::detectFiducialsServiceCallback, this);
+        }
+
+        // Publisher for visualization/debugging
+        fiducials_marker_publisher_ = node_handle_.advertise<visualization_msgs::Marker>( "fiducial_markers", 0 );
+        img2D_pub_= image_transport_->advertise("image", 1);
+
+        synchronizer_received_ = false;
+
+        //dynamic_reconfigure::Server<cob_fiducials::fiducialsConfig>::CallbackType f;
+        //f = boost::bind(&CobFiducialsNode::dynamicReconfigureCallback, this, _1, _2);
+        //dynamic_reconfigure_server_.clearCallback();
+        //dynamic_reconfigure_server_.updateConfig(launch_reconfigure_config_);
+        //dynamic_reconfigure_server_.setCallback(f);
+
+        ROS_INFO("[fiducials] Initializing colored point cloud toolbox [OK]");
+        ROS_INFO("[fiducials] Up and running");
+        return true;
+    }
+
+    //void dynamicReconfigureCallback(cob_fiducials::fiducialsConfig &config, uint32_t level)
+    //{
+    //	<localVariable> = config.<dynamic_reconfigure_variable>;
+    //}
+
+
+    /// Subscribe to camera topics if not already done.
+    void connectCallback()
+    {
+        ROS_INFO("[fiducials] Subscribing to camera topics");
+
+        color_camera_image_sub_.subscribe(*image_transport_, "image_color", 1);
+        color_camera_info_sub_.subscribe(node_handle_, "camera_info", 1);
+
+        color_image_sub_sync_ = boost::shared_ptr<message_filters::Synchronizer<ColorImageSyncPolicy> >(new message_filters::Synchronizer<ColorImageSyncPolicy>(ColorImageSyncPolicy(3)));
+        color_image_sub_sync_->connectInput(color_camera_image_sub_, color_camera_info_sub_);
+        color_image_sub_sync_->registerCallback(boost::bind(&CobFiducialsNode::colorImageCallback, this, _1, _2));
+
+        sub_counter_++;
+        ROS_INFO("[fiducials] %i subscribers on camera topics [OK]", sub_counter_);
+    }
+
+    /// Unsubscribe from camera topics if possible.
+    void disconnectCallback()
+    {
+        if (sub_counter_ > 0)
+        {
+            ROS_INFO("[fiducials] Unsubscribing from camera topics");
+
+            color_camera_image_sub_.unsubscribe();
+            color_camera_info_sub_.unsubscribe();
+
+            sub_counter_--;
+            ROS_INFO("[fiducials] %i subscribers on camera topics [OK]", sub_counter_);
+        }
+    }
+
+    /// Callback is executed, when shared mode is selected
+    /// Left and right is expressed when facing the back of the camera in horizontal orientation.
+    void colorImageCallback(const sensor_msgs::ImageConstPtr& color_camera_data,
+                            const sensor_msgs::CameraInfoConstPtr& color_camera_info)
+    {
+        {
+            boost::mutex::scoped_lock lock( mutexQ_ );
+
+            ROS_DEBUG("[fiducials] color image callback");
+
+            if (camera_matrix_initialized_ == false)
+            {
+                camera_matrix_ = cv::Mat::zeros(3,3,CV_64FC1);
+                camera_matrix_.at<double>(0,0) = color_camera_info->K[0];
+                camera_matrix_.at<double>(0,2) = color_camera_info->K[2];
+                camera_matrix_.at<double>(1,1) = color_camera_info->K[4];
+                camera_matrix_.at<double>(1,2) = color_camera_info->K[5];
+                camera_matrix_.at<double>(2,2) = 1;
+
+                camera_matrix_initialized_ = true;
+            }
+
+            // Publish
+            color_image_8U3_ = cv_bridge_0_.imgMsgToCv(color_camera_data, "bgr8");
+
+            cv::Mat tmp = color_image_8U3_;
+            color_mat_8U3_ = tmp.clone();
+
+            if (ros_node_mode_ == MODE_TOPIC || ros_node_mode_ == MODE_TOPIC_AND_SERVICE)
+            {
+                cob_object_detection_msgs::DetectionArray detection_array;
+                detectFiducials(detection_array, color_mat_8U3_);
+
+                detect_fiducials_pub_.publish(detection_array);
+
+                cv_bridge::CvImage cv_ptr;
+                cv_ptr.image = color_mat_8U3_;
+                cv_ptr.encoding = CobFiducialsNode::color_image_encoding_;
+                img2D_pub_.publish(cv_ptr.toImageMsg());
+            }
+
+            synchronizer_received_ = true;
+
+            // Notify waiting thread
+        }
+        condQ_.notify_one();
+    }
+
+
+
+    bool detectFiducialsServiceCallback(cob_object_detection_msgs::DetectObjects::Request &req,
+                                        cob_object_detection_msgs::DetectObjects::Response &res)
+    {
+        ROS_DEBUG("[fiducials] Service Callback");
+
+        // Connect to image topics
+        bool result = false;
+        synchronizer_received_ = false;
+        connectCallback();
+
+        // Wait for data
+        {
+            boost::mutex::scoped_lock lock( mutexQ_);
+            boost::system_time const timeout=boost::get_system_time()+ boost::posix_time::milliseconds(5000);
+
+            ROS_INFO("[fiducials] Waiting for image data");
+            if (condQ_.timed_wait(lock, timeout))
+                ROS_INFO("[fiducials] Waiting for image data [OK]");
+            else
+            {
+                ROS_WARN("[fiducials] Could not receive image data from ApproximateTime synchronizer");
+                return false;
+            }
+
+            // Wait for data (at least 5 seconds)
+            //int nSecPassed = 0;
+            //float nSecIncrement = 0.5;
+            //while (!synchronizer_received_ && nSecPassed < 10)
+            //{
+            //	ros::Duration(nSecIncrement).sleep();
+            //	nSecPassed += nSecIncrement;
+            //	ROS_INFO("[fiducials] Waiting");
+            //}
+
+            //if (!synchronizer_received_)
+            //{
+            //	ROS_WARN("[fiducials] Could not receive image data");
+            //	return false;
+            //}
+
+
+            // Publish reprojection matrix
+            cob_object_detection_msgs::DetectionArray detection_array;
+            detectFiducials(detection_array, color_mat_8U3_);
+
+            detect_fiducials_pub_.publish(detection_array);
+
+            cv_bridge::CvImage cv_ptr;
+            cv_ptr.image = color_mat_8U3_;
+            cv_ptr.encoding = CobFiducialsNode::color_image_encoding_;
+            img2D_pub_.publish(cv_ptr.toImageMsg());
+
+            disconnectCallback();
+
+            return result;
+        }
+    }
+
+    bool detectFiducials(cob_object_detection_msgs::DetectionArray& detection_array, cv::Mat& color_image)
+    {
+
+
+        return true;
+    }
+
+    unsigned long RenderPose(cv::Mat& image, cv::Mat& rot_3x3_CfromO, cv::Mat& trans_3x1_CfromO)
+    {
+        cv::Mat object_center(3, 1, CV_64FC1);
+        double* p_object_center = object_center.ptr<double>(0);
+        p_object_center[0] = 0;
+        p_object_center[1] = 0;
+        p_object_center[2] = 0;
+
+        cv::Mat rot_inv = rot_3x3_CfromO.inv();
+
+        // Compute coordinate axis for visualization
+        cv::Mat pt_axis(4, 3, CV_64FC1);
+        double* p_pt_axis = pt_axis.ptr<double>(0);
+        p_pt_axis[0] = 0 + p_object_center[0];
+        p_pt_axis[1] = 0 + p_object_center[1];
+        p_pt_axis[2] = 0 + p_object_center[2];
+        p_pt_axis = pt_axis.ptr<double>(1);
+        p_pt_axis[0] = 0.1 + p_object_center[0];
+        p_pt_axis[1] = 0 + p_object_center[1];
+        p_pt_axis[2] = 0 + p_object_center[2];
+        p_pt_axis = pt_axis.ptr<double>(2);
+        p_pt_axis[0] = 0 + p_object_center[0];
+        p_pt_axis[1] = 0.1 + p_object_center[1];
+        p_pt_axis[2] = 0 + p_object_center[2];
+        p_pt_axis = pt_axis.ptr<double>(3);
+        p_pt_axis[0] = 0 + p_object_center[0];
+        p_pt_axis[1] = 0 + p_object_center[1];
+        p_pt_axis[2] = 0.1 + p_object_center[2];
+
+        // Transform data points
+        std::vector<cv::Point> vec_2d(4, cv::Point());
+        for (int i=0; i<4; i++)
+        {
+            cv::Mat vec_3d = pt_axis.row(i).clone();
+            vec_3d = vec_3d.t();
+            vec_3d = rot_3x3_CfromO*vec_3d;
+            vec_3d += trans_3x1_CfromO;
+            double* p_vec_3d = vec_3d.ptr<double>(0);
+
+            ReprojectXYZ(p_vec_3d[0], p_vec_3d[1], p_vec_3d[2],
+                         vec_2d[i].x , vec_2d[i].y);
+        }
+
+        // Render results
+        int line_width = 1;
+        cv::line(image, vec_2d[0], vec_2d[1], cv::Scalar(0, 0, 255), line_width);
+        cv::line(image, vec_2d[0], vec_2d[2], cv::Scalar(0, 255, 0), line_width);
+        cv::line(image, vec_2d[0], vec_2d[3], cv::Scalar(255, 0, 0), line_width);
+
+        return ipa_Utils::RET_OK;
+    }
+
+    unsigned long ReprojectXYZ(double x, double y, double z, int& u, int& v)
+    {
+        cv::Mat XYZ(3, 1, CV_64FC1);
+        cv::Mat UVW(3, 1, CV_64FC1);
+
+        double* d_ptr = 0;
+        double du = 0;
+        double dv = 0;
+        double dw = 0;
+
+        x *= 1000;
+        y *= 1000;
+        z *= 1000;
+
+        d_ptr = XYZ.ptr<double>(0);
+        d_ptr[0] = x;
+        d_ptr[1] = y;
+        d_ptr[2] = z;
+
+        UVW = camera_matrix_ * XYZ;
+
+        d_ptr = UVW.ptr<double>(0);
+        du = d_ptr[0];
+        dv = d_ptr[1];
+        dw = d_ptr[2];
+
+        u = cvRound(du/dw);
+        v = cvRound(dv/dw);
+
+        return ipa_Utils::RET_OK;
+    }
+
+    unsigned long loadParameters()
+    {
+        std::string tmp_string;
+
+        /// Parameters are set within the launch file
+        if (node_handle_.getParam("ros_node_mode", tmp_string) == false)
+        {
+            ROS_ERROR("[fiducials] Mode for fiducial node not specified");
+            return false;
+        }
+        if (tmp_string == "MODE_SERVICE")
+        {
+            ros_node_mode_ = CobFiducialsNode::MODE_SERVICE;
+        }
+        else if (tmp_string == "MODE_TOPIC")
+        {
+            ros_node_mode_ = CobFiducialsNode::MODE_TOPIC;
+        }
+        else if (tmp_string == "MODE_TOPIC_AND_SERVICE")
+        {
+            ros_node_mode_ = CobFiducialsNode::MODE_TOPIC_AND_SERVICE;
+        }
+        else
+        {
+            std::string str = "[fiducials] Mode '" + tmp_string + "' unknown, try 'MODE_SERVICE' or 'MODE_TOPIC'";
+            ROS_ERROR("%s", str.c_str());
+            return false;
+        }
+
+        ROS_INFO("ROS node mode: %s", tmp_string.c_str());
+
+        //if (node_handle_.getParam("StereoPreFilterCap", StereoPreFilterCap_) == false)
+        //{
+        //	ROS_ERROR("[sensor_fusion] StereoPreFilterCap for sensor fusion node not specified");
+        //	return false;
+        //}
+        //launch_reconfigure_config_.preFilterCap = StereoPreFilterCap_;
+
+        return ipa_Utils::RET_OK;
+    }
+};
+
+std::string CobFiducialsNode::color_image_encoding_ = "rgb8";
+}; // END namepsace
+//1: The namespace in which the Triangle plugin will live. Typically, we use the name of the package
+// that contains the library that Triangle is a part of. In this case, that's pluginlib_tutorials
+// which is the name of the package we created in step one of this tutorial.
+//2: The name we wish to give to the plugin.... we'll call ours regular_triangle.
+//3: The fully-qualified type of the plugin class, in this case, polygon_plugins::Triangle.
+//4: The fully-qualified type of the base class, in this case, polygon_base::RegularPolygon
+// plugin/nodelet namespace, plugin/nodelet name, qualified class name, qualified nodelete class name
+//PLUGINLIB_DECLARE_CLASS(cob_sensor_fusion, cob_sensor_fusion_nodelet, ipa_SensorFusion::CobSensorFusionNode, nodelet::Nodelet);
+
+//#######################
+//#### main programm ####
+int main(int argc, char** argv)
+{
+    /// initialize ROS, specify name of node
+    ros::init(argc, argv, "sensor_fusion");
+
+    /// Create a handle for this node, initialize node
+    ros::NodeHandle nh;
+
+    /// Create camera node class instance
+    ipa_Fiducials::CobFiducialsNode fiducials_node(nh);
+
+    ros::MultiThreadedSpinner spinner(2); // Use 4 threads
+    spinner.spin();
+    //	ros::spin();
+
+    return 0;
+}
