aaa59e97433df06b25c8853f59c65178fcf35e07
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2013-07-02 16:21:20 +0200

    added communication functions

---
+++ cob_object_recording/ros/include/cob_object_recording/object_recording.h
@@ -0,0 +1,82 @@
+/*
+ * object_recording.h
+ *
+ *  Created on: 02.07.2013
+ *      Author: rbormann
+ */
+
+#ifndef OBJECT_RECORDING_H_
+#define OBJECT_RECORDING_H_
+
+// ROS includes
+#include <ros/ros.h>
+//#include <ros/package.h>
+
+// ROS message includes
+#include <sensor_msgs/Image.h>
+//#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <cob_object_detection_msgs/DetectionArray.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
+// boost
+#include <boost/bind.hpp>
+
+// PCL
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+// opencv
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+
+class ObjectRecording
+{
+public:
+
+	ObjectRecording();
+	ObjectRecording(ros::NodeHandle nh);
+
+	~ObjectRecording();
+
+protected:
+	/// callback for the incoming pointcloud data stream
+	void inputCallback(const cob_object_detection_msgs::DetectionArray::ConstPtr& input_marker_detections_msg, const sensor_msgs::PointCloud2::ConstPtr& input_pointcloud_msg, const sensor_msgs::Image::ConstPtr& input_image_msg);
+
+	/// Converts a color image message to cv::Mat format.
+	unsigned long convertColorImageMessageToMat(const sensor_msgs::Image::ConstPtr& image_msg, cv_bridge::CvImageConstPtr& image_ptr, cv::Mat& image);
+
+//	void calibrationCallback(const sensor_msgs::CameraInfo::ConstPtr& calibration_msg);
+
+	message_filters::Subscriber<cob_object_detection_msgs::DetectionArray> input_marker_detection_sub_;	///< detection of coordinate system the object is placed on
+//	ros::Subscriber input_pointcloud_sub_;	///< incoming point cloud topic
+	message_filters::Subscriber<sensor_msgs::PointCloud2> input_pointcloud_sub_;	///< incoming point cloud topic
+	ros::Subscriber input_pointcloud_camera_info_sub_;	///< camera calibration of incoming data
+	image_transport::ImageTransport* it_;
+	image_transport::SubscriberFilter color_image_sub_; ///< color camera image topic
+	message_filters::Synchronizer< message_filters::sync_policies::ApproximateTime<cob_object_detection_msgs::DetectionArray, sensor_msgs::PointCloud2, sensor_msgs::Image> >* sync_input_;
+
+	ros::NodeHandle node_handle_;			///< ROS node handle
+
+//	unsigned int pointcloud_width_;			///< width of the received point cloud
+//	unsigned int pointcloud_height_;			///< height of the received point cloud
+//	cv::Mat projection_matrix_;	///< projection matrix of the calibrated camera that transforms points from 3D to image plane in homogeneous coordinates: [u,v,w]=P*[X,Y,Z,1]
+};
+
+#endif /* OBJECT_RECORDING_H_ */
---
+++ cob_object_recording/ros/src/object_recording.cpp
@@ -0,0 +1,107 @@
+#include <cob_object_recording/object_recording.h>
+#include <boost/filesystem.hpp>
+#include <fstream>
+
+ObjectRecording::ObjectRecording()
+{
+	it_ = 0;
+	sync_input_ = 0;
+}
+
+ObjectRecording::ObjectRecording(ros::NodeHandle nh)
+: node_handle_(nh)
+{
+//	projection_matrix_ = (cv::Mat_<double>(3, 4) << 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+//	pointcloud_width_ = 640;
+//	pointcloud_height_ = 480;
+
+	// subscribers
+	input_marker_detection_sub_.subscribe(node_handle_, "input_marker_detections", 1);
+	it_ = new image_transport::ImageTransport(node_handle_);
+	color_image_sub_.subscribe(*it_, "input_color_image", 1);
+	//input_pointcloud_sub_ = node_handle_.subscribe("input_pointcloud_segments", 10, &ObjectRecording::inputCallback, this);
+	input_pointcloud_sub_.subscribe(node_handle_, "input_pointcloud", 1);
+//	input_pointcloud_camera_info_sub_ = node_handle_.subscribe("input_pointcloud_camera_info", 1, &ObjectRecording::calibrationCallback, this);
+
+	// input synchronization
+	sync_input_ = new message_filters::Synchronizer< message_filters::sync_policies::ApproximateTime<cob_object_detection_msgs::DetectionArray, sensor_msgs::PointCloud2, sensor_msgs::Image> >(1);
+	sync_input_->connectInput(input_marker_detection_sub_, input_pointcloud_sub_, color_image_sub_);
+	sync_input_->registerCallback(boost::bind(&ObjectRecording::inputCallback, this, _1, _2, _3));
+}
+
+ObjectRecording::~ObjectRecording()
+{
+
+	if (it_ != 0) delete it_;
+	if (sync_input_ != 0) delete sync_input_;
+}
+
+/// callback for the incoming pointcloud data stream
+void ObjectRecording::inputCallback(const cob_object_detection_msgs::DetectionArray::ConstPtr& input_marker_detections_msg, const sensor_msgs::PointCloud2::ConstPtr& input_pointcloud_msg, const sensor_msgs::Image::ConstPtr& input_image_msg)
+{
+	std::cout << "Recording data..." << std::endl;
+
+	// convert color image to cv::Mat
+	cv_bridge::CvImageConstPtr color_image_ptr;
+	cv::Mat color_image;
+	if (convertColorImageMessageToMat(input_image_msg, color_image_ptr, color_image) == false)
+		return;
+
+	typedef pcl::PointXYZRGB PointType;
+	pcl::PointCloud<PointType> input_pointcloud;
+	pcl::fromROSMsg(*input_pointcloud_msg, input_pointcloud);
+
+	cv::Mat display_segmentation = cv::Mat::zeros(input_pointcloud_msg->height, input_pointcloud_msg->width, CV_8UC3);
+	cv::imshow("color image", color_image);
+	cv::imshow("segmented image", display_segmentation);
+	cv::waitKey(10);
+}
+
+/// Converts a color image message to cv::Mat format.
+unsigned long ObjectRecording::convertColorImageMessageToMat(const sensor_msgs::Image::ConstPtr& image_msg, cv_bridge::CvImageConstPtr& image_ptr, cv::Mat& image)
+{
+	try
+	{
+		image_ptr = cv_bridge::toCvShare(image_msg, sensor_msgs::image_encodings::BGR8);
+	}
+	catch (cv_bridge::Exception& e)
+	{
+		ROS_ERROR("ObjectCategorization: cv_bridge exception: %s", e.what());
+		return false;
+	}
+	image = image_ptr->image;
+
+	return true;
+}
+
+//void ObjectRecording::calibrationCallback(const sensor_msgs::CameraInfo::ConstPtr& calibration_msg)
+//{
+//	pointcloud_height_ = calibration_msg->height;
+//	pointcloud_width_ = calibration_msg->width;
+//	cv::Mat temp(3,4,CV_64FC1);
+//	for (int i=0; i<12; i++)
+//		temp.at<double>(i/4,i%4) = calibration_msg->P.at(i);
+////		std::cout << "projection_matrix: [";
+////		for (int v=0; v<3; v++)
+////			for (int u=0; u<4; u++)
+////				std::cout << temp.at<double>(v,u) << " ";
+////		std::cout << "]" << std::endl;
+//	projection_matrix_ = temp;
+//}
+
+
+int main (int argc, char** argv)
+{
+	// Initialize ROS, specify name of node
+	ros::init(argc, argv, "object_recording");
+
+	// Create a handle for this node, initialize node
+	ros::NodeHandle nh;
+
+	// Create and initialize an instance of ObjectRecording
+	ObjectRecording objectRecording(nh);
+
+	ros::spin();
+
+	return (0);
+}
