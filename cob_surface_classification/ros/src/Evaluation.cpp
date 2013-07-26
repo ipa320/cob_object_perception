0a74f243b04ca19162c9671c0a15d5932d814554
ipa-rmb-ce <carolin.eckard@ipa.fraunhofer.de>
2013-07-26 14:46:58 +0200

    evaluation files added, not tested yet

---
+++ cob_surface_classification/ros/src/Evaluation.cpp
@@ -0,0 +1,63 @@
+/*
+ * Evaluation.cpp
+ *
+ *  Created on: Jul 10, 2013
+ *      Author: rmb-ce
+ */
+
+#include "Evaluation.h"
+
+
+Evaluation::Evaluation() {
+	search_directory = std::string(getenv("HOME")) + "/records/";
+
+	color_tab.resize(NUM_LABELS);
+	color_tab[I_EDGE] = EVAL_COL_EDGE;
+	color_tab[I_PLANE] = EVAL_COL_PLANE;
+	color_tab[I_CONCAVE]= EVAL_COL_CONC;
+	color_tab[I_CONVEX]= EVAL_COL_CONV;
+}
+
+Evaluation::~Evaluation() {
+}
+
+int Evaluation::compareClassification(std::string gt_filename)
+{
+
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr gt (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (search_directory + gt_filename, *gt) == -1)
+	{
+		PCL_ERROR ("Couldn't read gt-file \n");
+		return (-1);
+	}
+
+	compareClassification(gt);
+}
+
+
+
+int Evaluation::compareClassification(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr gt)
+{
+	int countCorrect = 0;
+	int countCompared = 0;
+
+	//comparison
+	ST::CH::ClusterPtr c_it,c_end;
+	for ( boost::tie(c_it,c_end) = clusterHandler->getClusters(); c_it != c_end; ++c_it)
+	{
+		if(c_it->type == I_EDGE || c_it->type == I_PLANE || c_it->type == I_CONCAVE || c_it->type == I_CONVEX )
+		{
+			uint32_t rgb = color_tab[c_it->type];
+			for (ST::CH::ClusterType::iterator idx=c_it->begin(); idx != c_it->end(); ++idx)
+			{
+				countCompared++;
+				if(gt->points[*idx].rgb == *reinterpret_cast<float*>(&rgb))
+					countCorrect++;
+			}
+		}
+	}
+	std::cout << "compared points: "<< countCompared << ", correctly classified points: " << countCorrect << "\n";
+	return 0;
+}
---
+++ cob_surface_classification/ros/src/Evaluation.h
@@ -0,0 +1,49 @@
+/*
+ * Evaluation.h
+ *
+ *  Created on: Jul 10, 2013
+ *      Author: rmb-ce
+ */
+
+#ifndef EVALUATION_H_
+#define EVALUATION_H_
+
+//package includes
+#include "cob_3d_segmentation/cluster_handler.h"
+#include <cob_3d_segmentation/cluster_classifier.h>
+#include <cob_3d_segmentation/depth_segmentation.h>
+#include <cob_3d_mapping_common/label_defines.h>
+
+//pcl
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+
+
+#include <boost/tuple/tuple.hpp>
+
+
+class Evaluation {
+public:
+	Evaluation();
+	virtual ~Evaluation();
+
+	typedef cob_3d_segmentation::PredefinedSegmentationTypes ST;
+    inline void setClusterHandler(ST::CH::Ptr cHdl) { clusterHandler = cHdl; }
+
+	int compareClassification(std::string gt_filename);
+	int compareClassification(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr gt);
+
+private:
+	std::string search_directory;
+
+
+	ST::CH::Ptr clusterHandler;
+
+    std::vector<int> color_tab;
+
+
+
+
+};
+
+#endif /* EVALUATION_H_ */
