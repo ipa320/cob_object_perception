dd4ff2b61632e81b9526e6c33262d67db2f5648f
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2014-03-05 13:27:28 +0100

    version with 3 steps for line approximation width

--- cob_surface_classification/ros/src/surface_classification_node.cpp
+++ cob_surface_classification/ros/src/surface_classification_node.cpp
@@ -253,10 +253,8 @@ public:
 
 
 		cv::Mat x_dx, y_dy, z_dx, z_dy;
-		cv::Mat average_dz_right = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC1);
-		cv::Mat average_dz_left = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC1);
-		cv::Mat average_dx_right = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC1);
-		cv::Mat average_dx_left = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC1);
+		cv::Mat average_dz = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC3);
+		cv::Mat average_dx = cv::Mat::zeros(z_image.rows, z_image.cols, CV_32FC3);
 		cv::Mat edge = cv::Mat::zeros(z_image.rows, z_image.cols, CV_8UC1);
 		//cv::medianBlur(depth_image, depth_image, 5);
 		cv::Sobel(x_image, x_dx, -1, 1, 0, 5, 1./(6.*16.));
@@ -279,82 +277,70 @@ public:
 //			std::cout << std::endl;
 //		}
 		int max_line_width = 30;
-		int line_width = 10;		// 1px/0.10m
-		int last_line_width = 10;
-		for (int v=max_line_width; v<z_dx.rows-max_line_width-1; ++v)
+		int line_width = 10;
+		for (int v=0; v<z_dx.rows-max_line_width-1; ++v)
 		{
-			for (int u=max_line_width; u<z_dx.cols-max_line_width-1; ++u)
+			for (int u=0; u<z_dx.cols-max_line_width-1; ++u)
 			{
-				// depth dependent scan line width for slope computation (1px/0.10m)
-				line_width = std::min(int(10*z_image.at<float>(v,u)), max_line_width);
-				if (line_width == 0)
-					line_width = last_line_width;
-				else
-					last_line_width = line_width;
-
-				double avg_dz = 0., avg_dx = 0.;
+				double sum_dz = 0., sum_dx = 0.;
+				double avg_dz_10 = 1e30, avg_dz_20 = 1e30;
+				double avg_dx_10 = 1e30, avg_dx_20 = 1e30;
 				int number_values = 0;
-				for (int i=-line_width; i<0; ++i)
+				for (int i=0; i<max_line_width; ++i)
 				{
 					float x_val = x_dx.at<float>(v,u+i);
 					float z_val = z_dx.at<float>(v,u+i);
 					if (x_val > 0. && z_val > -0.05f && z_val < 0.05f)
 					{
-						avg_dz += z_val;
-						avg_dx += x_val;
+						sum_dz += z_val;
+						sum_dx += x_val;
 						++number_values;
 					}
 					// else jump edge
-				}
-				//std::cout << avg_slope/(double)number_values << "\t";
-				if (number_values > 0)
-				{
-					average_dz_left.at<float>(v,u) = avg_dz/(double)number_values;
-					average_dx_left.at<float>(v,u) = avg_dx/(double)number_values;
-				}
-
-				avg_dz = 0.; avg_dx = 0.;
-				number_values = 0;
-				for (int i=1; i<=line_width; ++i)
-				{
-//					pcl::PointXYZRGB& point1 = cloud->at(u+i, v);
-//					pcl::PointXYZRGB& point2 = cloud->at(u+i+1, v);
-//					float val = (point2.z-point1.z);
-//					if (point2.x-point1.x > 0. && val > -0.05f && val < 0.05f)
-
-					//float val = (point2.z-point1.z)/(point2.x-point1.x);
-					//float val = dx.at<float>(v,u+i);
-					//if (val > -0.05 && val < 0.05)
-
-					float x_val = x_dx.at<float>(v,u+i);
-					float z_val = z_dx.at<float>(v,u+i);
-					if (x_val > 0. && z_val > -0.05f && z_val < 0.05f)
+					if (i==9 && number_values > 0)
 					{
-						avg_dz += z_val;
-						avg_dx += x_val;
-						++number_values;
+						avg_dz_10 = sum_dz/(double)number_values;
+						avg_dx_10 = sum_dx/(double)number_values;
 					}
-					// else jump edge
+					if (i==19 && number_values > 0)
+					{
+						avg_dz_20 = sum_dz/(double)number_values;
+						avg_dx_20 = sum_dx/(double)number_values;
+					}
 				}
 				//std::cout << avg_slope/(double)number_values << "\t";
 				if (number_values > 0)
 				{
-					average_dz_right.at<float>(v,u) = avg_dz/(double)number_values;
-					average_dx_right.at<float>(v,u) = avg_dx/(double)number_values;
+					cv::Vec3f vals_dz(0,0,0), vals_dx(0,0,0);
+					if (avg_dz_10 != 1e30)
+					{
+						vals_dz.val[0] = avg_dz_10;
+						vals_dx.val[0] = avg_dx_10;
+					}
+					if (avg_dz_20 != 1e30)
+					{
+						vals_dz.val[1] = avg_dz_20;
+						vals_dx.val[1] = avg_dx_20;
+					}
+					vals_dz.val[2] = sum_dz/(double)number_values;
+					vals_dx.val[2] = sum_dx/(double)number_values;
+					average_dz.at<cv::Vec3f>(v,u) = vals_dz;
+					average_dx.at<cv::Vec3f>(v,u) = vals_dx;
 				}
 
 				if (z_dx.at<float>(v,u) <= -0.05 || z_dx.at<float>(v,u) >= 0.05)
 					edge.at<uchar>(v,u) = 255;
 			}
 		}
-		for (int v=max_line_width; v<average_dz_right.rows-max_line_width-1; ++v)
+		for (int v=max_line_width; v<average_dz.rows-max_line_width-1; ++v)
 		{
-			for (int u=max_line_width; u<average_dz_right.cols-max_line_width-1; ++u)
+			for (int u=max_line_width; u<average_dz.cols-max_line_width-1; ++u)
 			{
 				if (edge.at<uchar>(v,u) != 255)
 				{
-					double alpha_left = atan2(-average_dz_left.at<float>(v,u), -average_dx_left.at<float>(v,u));
-					double alpha_right = atan2(average_dz_right.at<float>(v,u), average_dx_right.at<float>(v,u));
+					int index = std::max(0, std::min(2, (int)(z_image.at<float>(v,u)-0.5f)));
+					double alpha_left = atan2(-average_dz.at<cv::Vec3f>(v,u-(1+index)*line_width).val[index], -average_dx.at<cv::Vec3f>(v,u-(1+index)*line_width).val[index]);
+					double alpha_right = atan2(average_dz.at<cv::Vec3f>(v,u+1).val[index], average_dx.at<cv::Vec3f>(v,u+1).val[index]);
 					//if (v==240 && u>310 && u<330)
 					//	std::cout << "al: " << alpha_left << "   ar: " << alpha_right << "   diff: " << fabs(alpha_left-alpha_right) << "<" << 160./180.*CV_PI << "?\t";
 					if (fabs(alpha_left-alpha_right) < 145./180.*CV_PI || fabs(alpha_left-alpha_right) > 215./180.*CV_PI)
@@ -366,8 +352,8 @@ public:
 		cv::normalize(x_dx, x_dx, 0., 1., cv::NORM_MINMAX);
 		cv::imshow("x_dx", x_dx);
 		//cv::normalize(average_slope, average_slope, 0., 1., cv::NORM_MINMAX);
-		average_dz_right = average_dz_right * 15 + 0.5;
-		cv::imshow("average_slope", average_dz_right);
+		average_dz = average_dz * 15 + 0.5;
+		cv::imshow("average_slope", average_dz);
 		cv::imshow("edge", edge);
 		cv::waitKey(10);
 		return;
