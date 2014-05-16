9f39fc6c1b449e857d8f8e6bc9d6e548dbcc895f
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2014-05-16 10:56:36 +0200

    refactoring finished, added a publisher for the segmented point cloud data

--- cob_surface_classification/ros/src/surface_classification_node.cpp
+++ cob_surface_classification/ros/src/surface_classification_node.cpp
@@ -77,7 +77,7 @@
 #define SEG_WITHOUT_EDGES_VIS 		false 	//visualisation of segmentation without edge image
 #define CLASS_VIS 					false 	//visualisation of classification
 
-
+#define PUBLISH_SEGMENTATION		true	//publish segmented point cloud on topic
 
 
 // ROS includes
@@ -86,6 +86,7 @@
 // ROS message includes
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <cob_surface_classification/SegmentedPointCloud2.h>
 
 // topics
 #include <message_filters/subscriber.h>
@@ -160,6 +161,8 @@ public:
 		sync_input_ = new message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> >(30);
 		sync_input_->connectInput(colorimage_sub_, pointcloud_sub_);
 		sync_input_->registerCallback(boost::bind(&SurfaceClassificationNode::inputCallback, this, _1, _2));
+
+		segmented_pointcloud_pub_ = node_handle_.advertise<cob_surface_classification::SegmentedPointCloud2>("segmented_pointcloud", 1);
 	}
 
 	~SurfaceClassificationNode()
@@ -184,167 +187,6 @@ public:
 		}
 		image = image_ptr->image;
 	}
-
-//	// from https://gist.github.com/volkansalma/2972237
-//	//  or  http://lists.apple.com/archives/perfoptimization-dev/2005/Jan/msg00051.html
-//	#define PI_FLOAT     3.14159265f
-//	#define PIBY2_FLOAT  1.5707963f
-//	// |error| < 0.005
-//	float fast_atan2f_1(float y, float x)
-//	{
-//		if (x == 0.0f)
-//		{
-//			if (y > 0.0f) return PIBY2_FLOAT;
-//			if (y == 0.0f) return 0.0f;
-//			return -PIBY2_FLOAT;
-//		}
-//		float atan;
-//		float z = y/x;
-//		if (fabsf(z) < 1.0f)
-//		{
-//			atan = z/(1.0f + 0.28f*z*z);
-//			if (x < 0.0f)
-//			{
-//				if (y < 0.0f) return atan - PI_FLOAT;
-//				return atan + PI_FLOAT;
-//			}
-//		}
-//		else
-//		{
-//			atan = PIBY2_FLOAT - z/(z*z + 0.28f);
-//			if ( y < 0.0f ) return atan - PI_FLOAT;
-//		}
-//		return atan;
-//	}
-//
-//	float fast_atan2f_2(float y, float x)
-//	{
-//		//http://pubs.opengroup.org/onlinepubs/009695399/functions/atan2.html
-//		//Volkan SALMA
-//
-//		const float ONEQTR_PI = M_PI / 4.0;
-//		const float THRQTR_PI = 3.0 * M_PI / 4.0;
-//		float r, angle;
-//		float abs_y = fabs(y) + 1e-10f; // kludge to prevent 0/0 condition
-//		if (x < 0.0f)
-//		{
-//			r = (x + abs_y) / (abs_y - x);
-//			angle = THRQTR_PI;
-//		}
-//		else
-//		{
-//			r = (x - abs_y) / (x + abs_y);
-//			angle = ONEQTR_PI;
-//		}
-//		angle += (0.1963f * r * r - 0.9817f) * r;
-//		if (y < 0.0f)
-//			return (-angle); // negate if in quad III or IV
-//		else
-//			return (angle);
-//	}
-//
-//	float fast_arccosf(float x)
-//	{
-//		float x2 = x*x;
-//		float x4 = x2*x2;
-//		return (CV_PI/2.0 - (x + 1./6.*x*x2 + 3./40.*x*x4));
-//	}
-
-//	// creates an integral image within x-direction (i.e. line-wise, horizontally) for two source images
-//	void computeIntegralImageX(const cv::Mat& srcX, cv::Mat& dstX, const cv::Mat& srcZ, cv::Mat& dstZ)
-//	{
-//		dstX = cv::Mat(srcX.rows, srcX.cols, CV_32FC1);
-//		dstZ = cv::Mat(srcX.rows, srcX.cols, CV_32FC1);
-//		for (int v=0; v<srcX.rows; ++v)
-//		{
-//			float* dstX_ptr = (float*)dstX.ptr(v);
-//			const float* srcX_ptr = (const float*)srcX.ptr(v);
-//			float* dstZ_ptr = (float*)dstZ.ptr(v);
-//			const float* srcZ_ptr = (const float*)srcZ.ptr(v);
-//			float sumX = 0.f;
-//			float sumZ = 0.f;
-//			for (int u=0; u<srcX.cols; ++u)
-//			{
-//				if (*srcX_ptr > 0.f)
-//				{
-//					sumX += *srcX_ptr;
-//					sumZ += *srcZ_ptr;
-//				}
-//				*dstX_ptr = sumX;
-//				srcX_ptr++;
-//				dstX_ptr++;
-//				*dstZ_ptr = sumZ;
-//				srcZ_ptr++;
-//				dstZ_ptr++;
-//			}
-//		}
-//	}
-//
-//	// creates an integral image within y-direction (i.e. column-wise, vertically) for two source images
-//	void computeIntegralImageY(const cv::Mat& srcY, cv::Mat& dstY, const cv::Mat& srcZ, cv::Mat& dstZ)
-//	{
-//		dstY = cv::Mat(srcY.rows, srcY.cols, CV_32FC1);
-//		dstZ = cv::Mat(srcY.rows, srcY.cols, CV_32FC1);
-//		float* dstY_ptr = (float*)dstY.ptr(0);
-//		const float* srcY_ptr = (const float*)srcY.ptr(0);
-//		float* dstZ_ptr = (float*)dstZ.ptr(0);
-//		const float* srcZ_ptr = (const float*)srcZ.ptr(0);
-//		// copy first line
-//		for (int u=0; u<srcY.cols; ++u)
-//		{
-//			*dstY_ptr = *srcY_ptr;
-//			dstY_ptr++; srcY_ptr++;
-//			*dstZ_ptr = *srcZ_ptr;
-//			dstZ_ptr++; srcZ_ptr++;
-//		}
-//		// process remainder
-//		for (int v=1; v<srcY.rows; ++v)
-//		{
-//			float* dstY_ptr = (float*)dstY.ptr(v);
-//			float* dstYprev_ptr = (float*)dstY.ptr(v-1);
-//			const float* srcY_ptr = (const float*)srcY.ptr(v);
-//			float* dstZ_ptr = (float*)dstZ.ptr(v);
-//			float* dstZprev_ptr = (float*)dstZ.ptr(v-1);
-//			const float* srcZ_ptr = (const float*)srcZ.ptr(v);
-//			for (int u=0; u<srcY.cols; ++u)
-//			{
-//				if (*srcY_ptr > 0.f)
-//				{
-//					*dstY_ptr = *dstYprev_ptr + *srcY_ptr;
-//					*dstZ_ptr = *dstZprev_ptr + *srcZ_ptr;
-//				}
-//				else
-//				{
-//					*dstY_ptr = *dstYprev_ptr;
-//					*dstZ_ptr = *dstZprev_ptr;
-//				}
-//				srcY_ptr++;
-//				dstY_ptr++;
-//				dstYprev_ptr++;
-//				srcZ_ptr++;
-//				dstZ_ptr++;
-//				dstZprev_ptr++;
-//			}
-//		}
-//	}
-
-//	int isign(int x)
-//	{
-//		if (x==0)
-//			return 0;
-//		else if (x<0)
-//			return 1;
-//		return -1;
-//	}
-//
-//	int sign(int x)
-//	{
-//		if (x==0)
-//			return 0;
-//		else if (x<0)
-//			return -1;
-//		return 1;
-//	}
 
 	void inputCallback(const sensor_msgs::Image::ConstPtr& color_image_msg, const sensor_msgs::PointCloud2::ConstPtr& pointcloud_msg)
 	{
@@ -391,251 +233,6 @@ public:
 		//----------------------------------------
 
 		Timer tim;
-/*
-		//compute depth_image: greyvalue represents depth z
-		Timer tim;
-		tim.start();
-		cv::Mat x_image = cv::Mat::zeros(cloud->height, cloud->width, CV_32FC1);
-		cv::Mat y_image = cv::Mat::zeros(cloud->height, cloud->width, CV_32FC1);
-		cv::Mat z_image = cv::Mat::zeros(cloud->height, cloud->width, CV_32FC1);
-		int i=0;
-		for (unsigned int v=0; v<cloud->height; v++)
-		{
-			float* x_ptr = (float*)x_image.ptr(v);
-			float* y_ptr = (float*)y_image.ptr(v);
-			float* z_ptr = (float*)z_image.ptr(v);
-			for (unsigned int u=0; u<cloud->width; u++, i++)
-			{
-				//matrix indices: row y, column x!
-				pcl::PointXYZRGB& point = cloud->at(u,v);
-
-				if(point.z == point.z)	//test nan
-				{
-					*x_ptr = point.x;
-					*y_ptr = point.y;
-					*z_ptr = point.z;
-					//z_image.at<float>(v,u) = point.z;
-				}
-				++x_ptr;
-				++y_ptr;
-				++z_ptr;
-			}
-		}
-		//std::cout << "Time for x/y/z images: " << tim.getElapsedTimeInMilliSec() << "\n";
-		runtime_depth_image_ += tim.getElapsedTimeInMilliSec();
-
-		//visualization
-//		cv::Mat depth_im_scaled;
-//		cv::normalize(z_image, depth_im_scaled,0,1,cv::NORM_MINMAX);
-//		cv::imshow("depth_image", depth_im_scaled);
-//		cv::waitKey(10);
-
-		//draw crossline
-		//int lineLength = 20;
-		//cv::line(color_image,cv::Point2f(2*depth_image.cols/3 -lineLength/2, 2*depth_image.rows/3),cv::Point2f(2*depth_image.cols/3 +lineLength/2, 2*depth_image.rows/3),CV_RGB(0,1,0),1);
-		//cv::line(color_image,cv::Point2f(2*depth_image.cols/3 , 2*depth_image.rows/3 +lineLength/2),cv::Point2f(2*depth_image.cols/3 , 2*depth_image.rows/3 -lineLength/2),CV_RGB(0,1,0),1);
-
-		Timer total;
-		total.start();
-		tim.start();
-		cv::Mat x_dx, y_dy, z_dx, z_dy;
-		//cv::medianBlur(z_image, z_image, 5);
-		cv::Sobel(x_image, x_dx, -1, 1, 0, 5, 1./(6.*16.));
-		cv::Sobel(y_image, y_dy, -1, 0, 1, 5, 1./(6.*16.));
-		cv::Sobel(z_image, z_dx, -1, 1, 0, 5, 1./(6.*16.));
-		cv::Sobel(z_image, z_dy, -1, 0, 1, 5, 1./(6.*16.));
-		//cv::medianBlur(z_dx, z_dx, 5);
-		//cv::medianBlur(z_dy, z_dy, 5);
-		cv::GaussianBlur(z_dx, z_dx, cv::Size(5,5), 0, 0);
-		cv::GaussianBlur(z_dy, z_dy, cv::Size(5,5), 0, 0);
-		//std::cout << "Time for slope Sobel: " << tim.getElapsedTimeInMilliSec() << "\n";
-		runtime_sobel_ += tim.getElapsedTimeInMilliSec();
-
-		tim.start();
-//		cv::Mat kx, ky;
-//		cv::getDerivKernels(kx, ky, 1, 0, 5, false, CV_32F);
-//		std::cout << "kx:\n";
-//		for (int i=0; i<kx.rows; ++i)
-//		{
-//			for (int j=0; j<kx.cols; ++j)
-//				std::cout << kx.at<float>(i,j) << "\t";
-//			std::cout << std::endl;
-//		}
-//		std::cout << "\nky:\n";
-//		for (int i=0; i<ky.rows; ++i)
-//		{
-//			for (int j=0; j<ky.cols; ++j)
-//				std::cout << ky.at<float>(i,j) << "\t";
-//			std::cout << std::endl;
-//		}
-
-		// depth discontinuities
-		cv::Mat edge = cv::Mat::zeros(z_image.rows, z_image.cols, CV_8UC1);
-		const int max_line_width = 30;
-		for (int v = max_line_width; v < z_dx.rows - max_line_width - 1; ++v)
-		{
-			for (int u = max_line_width; u < z_dx.cols - max_line_width - 1; ++u)
-			{
-				float depth = z_image.at<float>(v, u);
-				if (depth==0.f)
-					continue;
-				if (z_dx.at<float>(v, u) <= -0.02*depth || z_dx.at<float>(v, u) >= 0.02*depth ||
-						z_dy.at<float>(v, u) <= -0.02*depth || z_dy.at<float>(v, u) >= 0.02*depth)
-					edge.at<uchar>(v, u) = 255;
-			}
-		}
-		cv::Mat edge_integral;
-		cv::integral(edge, edge_integral, CV_32S);
-		//std::cout << "Time for edge+integral: " << tim.getElapsedTimeInMilliSec() << "\n";
-
-		// surface discontinuities
-		// x lines
-		int line_width = 10; // 1px/0.10m
-		int last_line_width = 10;
-		cv::Mat x_dx_integralX, z_dx_integralX;
-		computeIntegralImageX(x_dx, x_dx_integralX, z_dx, z_dx_integralX);
-		for (int v = max_line_width; v < z_dx.rows - max_line_width - 1; ++v)
-		{
-			int edge_start_index = -1;
-			for (int u = max_line_width; u < z_dx.cols - max_line_width - 1; ++u)
-			{
-				float depth = z_image.at<float>(v, u);
-				if (depth==0.f)
-					continue;
-				//if (z_dx.at<float>(v, u) <= -0.05 || z_dx.at<float>(v, u) >= 0.05)
-//				if (z_dx.at<float>(v, u) <= -0.02*depth || z_dx.at<float>(v, u) >= 0.02*depth)
-//				{
-//					edge.at<uchar>(v, u) = 255;
-//				}
-//				else
-//				{
-					// depth dependent scan line width for slope computation (1px/0.10m)
-					line_width = std::min(int(10 * depth), max_line_width);
-					if (line_width == 0)
-						line_width = last_line_width;
-					else
-						last_line_width = line_width;
-					// do not compute if a depth discontinuity is on the line (ATTENTION: opencv uses a different indexing scheme which is basically +1 in x and y direction, so pixel itself is not included in sum)
-		//			if (edge_integral.at<int>(v+1,u+line_width+1)-edge_integral.at<int>(v+1,u-line_width)-edge_integral.at<int>(v,u+line_width+1)+edge_integral.at<int>(v,u-line_width) != 0)
-		//				continue;
-
-					// get average differences in x and z direction (ATTENTION: the integral images provide just the sum, not divided by number of elements, however, further processing only needs the sum, not the real average)
-					double avg_dx_l = x_dx_integralX.at<float>(v, u-1) - x_dx_integralX.at<float>(v, u-line_width);
-					double avg_dz_l = z_dx_integralX.at<float>(v, u-1) - z_dx_integralX.at<float>(v, u-line_width);
-					float avg_dx_r = x_dx_integralX.at<float>(v, u+line_width) - x_dx_integralX.at<float>(v, u+1);
-					float avg_dz_r = z_dx_integralX.at<float>(v, u+line_width) - z_dx_integralX.at<float>(v, u+1);
-
-					// estimate angle difference
-					float alpha_left = fast_atan2f_1(-avg_dz_l, -avg_dx_l);
-					float alpha_right = fast_atan2f_1(avg_dz_r, avg_dx_r);
-					float diff = fabs(alpha_left - alpha_right);
-					if (diff!=0 && (diff < 145. / 180. * CV_PI || diff > 215. / 180. * CV_PI))
-					{
-						//edge.at<uchar>(v, u) = 16; //(diff < 145. / 180. * CV_PI ? -64 : 64);//64 + 64 * 2 * fabs(CV_PI - fabs(alpha_left - alpha_right)) / CV_PI;
-						if (edge_start_index == -1)
-							edge_start_index = u;
-					}
-					else
-					{
-						if (edge_start_index != -1)
-						{
-							edge.at<uchar>(v, (edge_start_index+u-1)/2) = 255;	//192
-							edge_start_index = -1;
-						}
-					}
-//				}
-			}
-		}
-		// y lines
-		line_width = 10; // 1px/0.10m
-		last_line_width = 10;
-		cv::Mat y_dy_integralY, z_dy_integralY;
-		computeIntegralImageY(y_dy, y_dy_integralY, z_dy, z_dy_integralY);
-		std::vector<int> edge_start_index(z_dy.cols, -1);
-		for (int v = max_line_width; v < z_dy.rows - max_line_width - 1; ++v)
-		{
-			for (int u = max_line_width; u < z_dy.cols - max_line_width - 1; ++u)
-			{
-				float depth = z_image.at<float>(v, u);
-				if (depth==0.f)
-					continue;
-				//if (z_dy.at<float>(v, u) <= -0.05 || z_dy.at<float>(v, u) >= 0.05)
-//				if (z_dy.at<float>(v, u) <= -0.02*depth || z_dy.at<float>(v, u) >= 0.02*depth)
-//				{
-//					edge.at<uchar>(v, u) = 255;
-//				}
-//				else
-				if (edge.at<uchar>(v, u) == 0)
-				{
-					// depth dependent scan line width for slope computation (1px/0.10m)
-					line_width = std::min(int(10 * depth), max_line_width);
-					if (line_width == 0)
-						line_width = last_line_width;
-					else
-						last_line_width = line_width;
-					// do not compute if a depth discontinuity is on the line (ATTENTION: opencv uses a different indexing scheme which is basically +1 in x and y direction, so pixel itself is not included in sum)
-		//			if (edge_integral.at<int>(v+line_width+1,u+1)-edge_integral.at<int>(v-line_width,u+1)-edge_integral.at<int>(v+line_width+1,u)+edge_integral.at<int>(v-line_width,u) != 0)
-		//todo:				continue;
-
-					// get average differences in x and z direction (ATTENTION: the integral images provide just the sum, not divided by number of elements, however, further processing only needs the sum, not the real average)
-					double avg_dy_u = y_dy_integralY.at<float>(v-1, u) - y_dy_integralY.at<float>(v-line_width, u);
-					double avg_dz_u = z_dy_integralY.at<float>(v-1, u) - z_dy_integralY.at<float>(v-line_width, u);
-					float avg_dy_l = y_dy_integralY.at<float>(v+line_width, u) - y_dy_integralY.at<float>(v+1, u);
-					float avg_dz_l = z_dy_integralY.at<float>(v+line_width, u) - z_dy_integralY.at<float>(v+1, u);
-
-					// estimate angle difference
-					float alpha_upper = fast_atan2f_1(-avg_dz_u, -avg_dy_u);
-					float alpha_lower = fast_atan2f_1(avg_dz_l, avg_dy_l);
-					float diff = fabs(alpha_upper - alpha_lower);
-					if (diff!=0 && (diff < 145. / 180. * CV_PI || diff > 215. / 180. * CV_PI))
-					{
-						//edge.at<uchar>(v, u) = 32; //128 + (diff < 145. / 180. * CV_PI ? -64 : 64);//64 + 64 * 2 * fabs(CV_PI - fabs(alpha_left - alpha_right)) / CV_PI;
-						if (edge_start_index[u] == -1)
-							edge_start_index[u] = v;
-					}
-					else
-					{
-						if (edge_start_index[u] != -1)
-						{
-							edge.at<uchar>((edge_start_index[u]+v-1)/2, u) = 255;  //192;
-							edge_start_index[u] = -1;
-						}
-					}
-				}
-			}
-		}
-		cv::dilate(edge, edge, cv::Mat(), cv::Point(-1,-1), 1);
-		cv::erode(edge, edge, cv::Mat(), cv::Point(-1,-1), 1);
-		for (int v=0; v<z_image.rows; ++v)
-			for (int u=0; u<z_image.cols; ++u)
-				if (z_image.at<float>(v,u)==0)
-					edge.at<uchar>(v,u)=0;
-		//std::cout << "Time for slope+edge: " << tim.getElapsedTimeInMilliSec() << "\n";
-		runtime_edge_ += tim.getElapsedTimeInMilliSec();
-
-		// remaining problems:
-		// 1. some edges = double edges
-		// 2. noise -> speckle filter in the beginning?
-
-		runtime_total_ += total.getElapsedTimeInMilliSec();
-		++number_processed_images_;
-
-//		std::cout << "runtime_total: " << runtime_total_/(double)number_processed_images_ <<
-//					"\nruntime_depth_image: " << runtime_depth_image_/(double)number_processed_images_ <<
-//					"\nruntime_sobel: " << runtime_sobel_/(double)number_processed_images_ <<
-//					"\nruntime_edge: " << runtime_edge_/(double)number_processed_images_ << std::endl;
-					//"\nruntime_visibility: " << runtime_visibility_/(double)number_processed_images_ <<
-
-//		cv::imshow("z_dx", z_dx);
-//		cv::normalize(x_dx, x_dx, 0., 1., cv::NORM_MINMAX);
-//		cv::imshow("x_dx", x_dx);
-		//cv::normalize(average_slope, average_slope, 0., 1., cv::NORM_MINMAX);
-//		average_dz_right = average_dz_right * 15 + 0.5;
-//		cv::imshow("average_slope", average_dz_right);
-		cv::imshow("edge", edge);
-		key = cv::waitKey(10);
-//		return;
-*/
 
 		//std::cout << key <<endl;
 		//record if "e" is pressed while "image"-window is activated
@@ -682,7 +279,7 @@ public:
 			{
 				tim.start();
 				one_.setInputCloud(cloud);
-				one_.setPixelSearchRadius(8,2,2);	//call before calling computeMaskManually()!!!
+				one_.setPixelSearchRadius(4,2,2);	//call before calling computeMaskManually()!!!
 				//one_.computeMaskManually_increasing(cloud->width);
 				one_.computeMaskManually(cloud->width);
 				one_.computePointAngleLookupTable(16);
@@ -755,6 +352,7 @@ public:
 				viewer.setBackgroundColor (0.0, 0.0, 0);
 				pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(segmented);
 				viewer.addPointCloud<pcl::PointXYZRGB> (segmented,rgb,"seg");
+				//viewer.setCameraPosition()
 				while (!viewer.wasStopped ())
 				{
 					viewer.spinOnce();
@@ -777,8 +375,6 @@ public:
 					viewerWithoutEdges.spinOnce();
 				}
 			}
-
-			return;
 
 			if(SEG_REFINE)
 			{
@@ -808,7 +404,23 @@ public:
 				viewerRef.removePointCloud("segRef");
 			}
 
+			if (PUBLISH_SEGMENTATION)
+			{
+				cob_surface_classification::SegmentedPointCloud2 msg;
+				msg.pointcloud = *pointcloud_msg;
+				for (ST::Graph::ClusterPtr c = graph->clusters()->begin(); c != graph->clusters()->end(); ++c)
+				{
+					cob_surface_classification::Int32Array point_indices;
+					point_indices.array.resize(c->size());
+					int i=0;
+					for (ST::Graph::ClusterType::iterator it = c->begin(); it != c->end(); ++it, ++i)
+						point_indices.array[i] = *it;
+					msg.clusters.push_back(point_indices);
+				}
+				segmented_pointcloud_pub_.publish(msg);
+			}
 
+			return;
 
 			if(CLASSIFY|| EVALUATION_ONLINE_MODE)
 			{
@@ -868,6 +480,7 @@ private:
 	image_transport::SubscriberFilter colorimage_sub_; ///< Color camera image topic
 	message_filters::Subscriber<sensor_msgs::PointCloud2> pointcloud_sub_;
 	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> >* sync_input_;
+	ros::Publisher segmented_pointcloud_pub_;	// publisher for the segmented point cloud
 
 	//records
 	Scene_recording rec_;
