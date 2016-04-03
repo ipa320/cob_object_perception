a7b506116dd53ccb42565d6ce14a58408ecf043b
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2016-04-03 10:02:48 +0200

    some changes

--- cob_texture_categorization/ros/src/texture_categorization.cpp
+++ cob_texture_categorization/ros/src/texture_categorization.cpp
@@ -261,8 +261,9 @@ void TextCategorizationNode::attributeLearningDatabaseTestFarhadi()
 	AttributeLearning al;
 	cv::Mat base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix;
 	create_train_data::DataHierarchyType data_hierarchy;
+	std::vector<std::string> image_filenames;
 	const std::string feature_filename = feature_files_path + database_identifier + "_database.txt";
-	al.loadTextureDatabaseBaseFeatures(feature_filename, 9688, base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix, data_hierarchy, database_identifier);
+	al.loadTextureDatabaseBaseFeatures(feature_filename, 9688, base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix, data_hierarchy, image_filenames, database_identifier);
 	std::cout << "Loading base features, attributes and class hierarchy from file finished.\n";
 
 	CrossValidationParams cvp(CrossValidationParams::LEAVE_OUT_ONE_OBJECT_PER_CLASS, 20, 57);
@@ -375,7 +376,7 @@ void TextCategorizationNode::attributeLearningGeneratedDatabaseTestHandcrafted()
 	std::string generated_attributes_file_name = feature_files_path + "all_generated_attributes.txt";//"ipa_database_generated_class_attributes.txt";
 	cv::Mat generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix;
 	create_train_data::DataHierarchyType generated_attributes_data_hierarchy;
-	al.loadTextureDatabaseBaseFeatures(generated_attributes_file_name, 16, generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix, generated_attributes_data_hierarchy, database_identifier);
+	al.loadTextureDatabaseBaseFeatures(generated_attributes_file_name, 16, generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix, generated_attributes_data_hierarchy, image_filenames, database_identifier);
 	cv::Mat train_ = generated_attributes_17, labels_ = generated_attributes_class_label_matrix, training_data, labels;
 //	for (int r=0; r<labels_.rows; ++r)
 //		if (considered_classes_.find(labels_.at<float>(r)) != considered_classes_.end())
@@ -434,7 +435,7 @@ void TextCategorizationNode::crossValidationVerbalClassDescription()
 	if (method == FARHADI)
 	{
 		std::string data_file_name = feature_files_path + database_identifier + "_database.txt";
-		al.loadTextureDatabaseBaseFeatures(data_file_name, 9688, base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix, data_hierarchy, database_identifier);
+		al.loadTextureDatabaseBaseFeatures(data_file_name, 9688, base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix, data_hierarchy, image_filenames, database_identifier);
 	}
 	else
 	{
@@ -465,7 +466,7 @@ void TextCategorizationNode::crossValidationVerbalClassDescription()
 	std::string generated_attributes_file_name = feature_files_path + "ipa_database_generated_class_attributes.txt";
 	cv::Mat generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix;
 	create_train_data::DataHierarchyType generated_attributes_data_hierarchy;
-	al.loadTextureDatabaseBaseFeatures(generated_attributes_file_name, 16, generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix, generated_attributes_data_hierarchy, database_identifier);
+	al.loadTextureDatabaseBaseFeatures(generated_attributes_file_name, 16, generated_attributes_16, generated_attributes_17, generated_attributes_class_label_matrix, generated_attributes_data_hierarchy, image_filenames, database_identifier);
 	// do the cross validation
 	// todo: change file name to not overwrite 20fold data ml.save_computed_attribute_matrices(feature_files_path, computed_attribute_matrices);
 	ml.load_computed_attribute_matrices(feature_files_path, computed_attribute_matrices);
@@ -499,7 +500,7 @@ void TextCategorizationNode::attributeLearningDTDDatabaseTest()
 	const std::string path_database_main = package_path + "/common/files/texture_database_dtd-r1.0.1/";
 	const std::string path_database = path_database_main + "images/";			// path to database			//	std::string path_database = "/media/rmb/SAMSUNG/rmb/datasetTextur/texture_database/";		// path to database
 	const std::string path_to_cross_validation_sets = path_database_main + "labels/";
-	const std::string feature_files_path = package_path + "/common/files/data/cimpoi2014_sift/"; 		// path to save data
+	const std::string feature_files_path = package_path + "/common/files/data/farhadi2009/"; 		// path to save data
 
 	// compute 17 texture attributes on the ipa texture database
 	create_train_data database_data(2);
@@ -515,12 +516,18 @@ void TextCategorizationNode::attributeLearningDTDDatabaseTest()
 	train_ml ml;
 	AttributeLearning al;
 	std::cout << "Loading base features, attributes and class hierarchy from file ...\n";
-	database_data.load_texture_database_features(feature_files_path, base_feature_matrix, ground_truth_attribute_matrix, computed_attribute_matrix, class_label_matrix, data_hierarchy, image_filenames, database_identifier);
-	scaleNormalizeHandcraftedAttributes(computed_attribute_matrix, computed_attribute_matrix_rescaled);
+	// handcrafted, cimpoi
+//	database_data.load_texture_database_features(feature_files_path, base_feature_matrix, ground_truth_attribute_matrix, computed_attribute_matrix, class_label_matrix, data_hierarchy, image_filenames, database_identifier);
+//	scaleNormalizeHandcraftedAttributes(computed_attribute_matrix, computed_attribute_matrix_rescaled);
+//	for (int k=0; k<base_feature_matrix.cols; ++k)
+//		cv::normalize(base_feature_matrix.col(k), base_feature_matrix.col(k), 1., 0., cv::NORM_L2);
+	// farhadi
+	const std::string feature_filename = feature_files_path + database_identifier + "_database.txt";
+	al.loadTextureDatabaseBaseFeatures(feature_filename, 9688, base_feature_matrix, ground_truth_attribute_matrix, class_label_matrix, data_hierarchy, image_filenames, database_identifier);
 	std::cout << "Loading base features, attributes and class hierarchy from file finished.\n";
 
 	CrossValidationParams cvp(CrossValidationParams::DTD_SPLITS, 10, 47);
-	setSVMConfigurations(cvp, "attributes_handcrafted_cimpoi47");
+	setSVMConfigurations(cvp, "attributes_dtd_farhadi");
 	//cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 100*3760, 0.001, CvSVM::C_SVC, CvSVM::LINEAR, 0., 0.2, 1., 10., 0., 0.));
 
 	al.crossValidationDTD(cvp, path_to_cross_validation_sets, base_feature_matrix, ground_truth_attribute_matrix, data_hierarchy, image_filenames);
@@ -561,16 +568,36 @@ void TextCategorizationNode::setNNConfigurations(CrossValidationParams& cvp, con
 void TextCategorizationNode::setSVMConfigurations(CrossValidationParams& cvp, const std::string& experiment_key)
 {
 	// SVM
-	if (experiment_key.compare("attributes_handcrafted")==0)
+	if (experiment_key.compare("attributes_dtd_handcrafted")==0)
 	{
-		for (double nu=0.1; nu<0.91; nu+=0.1)
-			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::LINEAR, 0., 0.1, 0., 1., nu, 0.));
-		std::vector<double> values; values.push_back(0.01); values.push_back(0.05); values.push_back(0.1); values.push_back(0.5); values.push_back(1.0);
-		for (size_t gamma_index=0; gamma_index<values.size(); ++gamma_index)
-			for (double nu=0.1; nu<0.91; nu+=0.1)
-				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::RBF, 0., values[gamma_index], 0., 1., nu, 0.));
+//		for (double nu=0.1; nu<0.91; nu+=0.1)
+//			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::LINEAR, 0., 0.1, 0., 1., nu, 0.));
+//		std::vector<double> values; values.push_back(0.01); values.push_back(0.05); values.push_back(0.1); values.push_back(0.5); values.push_back(1.0);
+//		for (size_t gamma_index=0; gamma_index<values.size(); ++gamma_index)
+//			for (double nu=0.1; nu<0.91; nu+=0.1)
+//				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVC, CvSVM::RBF, 0., values[gamma_index], 0., 1., nu, 0.));
+
+		std::vector<double> C_values; C_values.push_back(0.001); C_values.push_back(0.005); C_values.push_back(0.01); C_values.push_back(0.05); C_values.push_back(0.1); C_values.push_back(0.5); C_values.push_back(1.0);
+//		std::vector<double> C_values; C_values.push_back(5.); C_values.push_back(10.); C_values.push_back(50.); C_values.push_back(100.);
+//		for (size_t C_index=0; C_index<C_values.size(); ++C_index)
+//			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 100*3760, 0.001, CvSVM::C_SVC, CvSVM::LINEAR, 0., 0.2, 1., C_values[C_index], 0., 0.));
+		std::vector<double> gamma_values; gamma_values.push_back(0.01); gamma_values.push_back(0.05); gamma_values.push_back(0.1); gamma_values.push_back(0.5); gamma_values.push_back(1.0); gamma_values.push_back(5.0); gamma_values.push_back(10.0);
+//		std::vector<double> gamma_values; gamma_values.push_back(0.5); gamma_values.push_back(1.0); gamma_values.push_back(5.0); gamma_values.push_back(10.0);
+		for (size_t gamma_index=0; gamma_index<gamma_values.size(); ++gamma_index)
+			for (size_t C_index=0; C_index<C_values.size(); ++C_index)
+				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 100*3760, 0.001, CvSVM::C_SVC, CvSVM::RBF, 0., gamma_values[gamma_index], 1., C_values[C_index], 0., 0.));
 	}
-	else if (experiment_key.compare("attributes_handcrafted_cimpoi47")==0)
+	else if (experiment_key.compare("attributes_dtd_farhadi")==0)
+	{
+		std::vector<double> C_values; C_values.push_back(1.0); //C_values.push_back(5.0); C_values.push_back(10.0); C_values.push_back(50.0); C_values.push_back(100.0);
+//		for (size_t C_index=0; C_index<C_values.size(); ++C_index)
+//			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 100*3760, 0.001, CvSVM::C_SVC, CvSVM::LINEAR, 0., 0.2, 1., C_values[C_index], 0., 0.));
+		std::vector<double> gamma_values; gamma_values.push_back(0.01); gamma_values.push_back(0.05); gamma_values.push_back(0.1); gamma_values.push_back(0.5); gamma_values.push_back(1.0); gamma_values.push_back(5.0); gamma_values.push_back(10.0);
+		for (size_t gamma_index=0; gamma_index<gamma_values.size(); ++gamma_index)
+			for (size_t C_index=0; C_index<C_values.size(); ++C_index)
+				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 100*3760, 0.001, CvSVM::C_SVC, CvSVM::RBF, 0., gamma_values[gamma_index], 1., C_values[C_index], 0., 0.));
+	}
+	else if (experiment_key.compare("attributes_dtd_cimpoi47")==0)
 	{
 //		for (double nu=0.1; nu<0.91; nu+=0.1)
 //			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::LINEAR, 0., 0.1, 0., 1., nu, 0.));
@@ -586,6 +613,15 @@ void TextCategorizationNode::setSVMConfigurations(CrossValidationParams& cvp, co
 //		for (size_t gamma_index=0; gamma_index<gamma_values.size(); ++gamma_index)
 //			for (size_t C_index=0; C_index<C_values.size(); ++C_index)
 //				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::C_SVC, CvSVM::RBF, 0., gamma_values[gamma_index], 1., C_values[C_index], 0., 0.));
+	}
+	else if (experiment_key.compare("attributes_handcrafted")==0)
+	{
+		for (double nu=0.1; nu<0.91; nu+=0.1)
+			cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::LINEAR, 0., 0.1, 0., 1., nu, 0.));
+		std::vector<double> values; values.push_back(0.01); values.push_back(0.05); values.push_back(0.1); values.push_back(0.5); values.push_back(1.0);
+		for (size_t gamma_index=0; gamma_index<values.size(); ++gamma_index)
+			for (double nu=0.1; nu<0.91; nu+=0.1)
+				cvp.ml_configurations_.push_back(MLParams(MLParams::SVM, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 1000, FLT_EPSILON, CvSVM::NU_SVR, CvSVM::RBF, 0., values[gamma_index], 0., 1., nu, 0.));
 	}
 	else if (experiment_key.compare("attributes_farhadi2009")==0)
 	{
